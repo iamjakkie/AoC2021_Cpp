d1ede0c1a36c78000d5a718df5869637e947d881
jakkie <jakubkielbasiewicz@gmail.com>
2022-01-28 00:11:18 +0100

    Day 2 solution

--- 2/solution.cpp
+++ 2/solution.cpp
@@ -4,35 +4,22 @@
 #include <sstream>
 #include <algorithm>
 
-enum class Order
-{
-    forward,
-    up,
-    down
-};
-
-bool isNumber(const std::string &s)
-{
-    return !s.empty() && std::all_of(s.begin(), s.end(), ::isdigit);
-}
-
 int main()
 {
     std::ifstream file("input.txt");
-    std::string str, order;
-    int val, position, depth = 0;
-    while (std::getline(file, str))
+    std::string str, order, skip;
+    int val, position, depth, aim = 0;
+    while (file >> order >> val)
     {
-        std::stringstream linestream(str);
-        std::getline(linestream, order, ' ');
-        linestream >> val;
         if(order == "forward"){
             position+=val;
+            int inc = aim*val;
+            depth+=inc;
         } else if (order == "up")
         {
-            depth-=val;
+            aim-=val;
         } else if (order == "down"){
-            depth+=val;
+            aim+=val;
         }
     }
     std::cout << "Position: " << std::to_string(position) << " depth: " << std::to_string(depth) << std::endl;
