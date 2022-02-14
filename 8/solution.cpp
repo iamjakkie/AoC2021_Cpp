cc20f9ba7692807d8313d304295cee8e31c6cc9a
jakkie <jakubkielbasiewicz@gmail.com>
2022-02-15 00:04:39 +0100

    Day 8 part 2 continued

--- 8/solution.cpp
+++ 8/solution.cpp
@@ -27,22 +27,31 @@ b    .  b    .  .    c  b    c  b    c
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <unordered_set>
 
-void setMapping(std::unordered_map<char, char>& mapping, std::string s_num){
+void setMapping(std::unordered_map<char, char>& mapping, std::unordered_set<char>& full, std::string s_num){
     int size = s_num.size();
     if(size == 4){
         mapping['b'] = s_num[0];
+        full.erase(s_num[0]);
         mapping['c'] = s_num[1];
+        full.erase(s_num[1]);
         mapping['d'] = s_num[2];
+        full.erase(s_num[2]);
         mapping['f'] = s_num[3];
+        full.erase(s_num[3]);
     }
     if(size == 3){
         mapping['a'] = s_num[0];
+        full.erase(s_num[0]);
     }
-    if(size == 8){
+    if(size == 7){
         mapping['e'] = s_num[4];
+        full.erase(s_num[4]);
+    } if(full.size() == 1){
+        mapping['g'] = *(full.begin());
     }
-    
+
 }
 // 1 - cf
 // 4 - bcdf
@@ -54,9 +63,10 @@ int main()
     std::fstream file;
     std::string line;
     file.open("input.txt", std::ios::in);
-    std::unordered_map<char, std::vector<char>> mapping;
+    std::unordered_map<char, char> mapping;
+    std::unordered_map<std::string, int> translate {}
     int count = 0;
-
+    std::unordered_set<char> full {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
     while (std::getline(file, line))
     {
         std::string input = line.substr(0, line.find("|") -1);
@@ -73,18 +83,16 @@ int main()
             {
                 if (s_num != "")
                 {
-                    setMapping(mapping,s_num);
+                    setMapping(mapping, full, s_num);
                 }
                 s_num = "";
             }
         }
-        setMapping(mapping,s_num);
+        setMapping(mapping,full, s_num);
     }
-    for(auto p: mapping){
-        std::cout << p.first << ": ";
-        for(auto c: p.second){
-            std::cout << c << ",";
-        }
-        std::cout << std::endl;
+
+
+    for(auto c: mapping){
+        std::cout << c.first << ": " << c.second << std::endl;
     }
 }
