ba0a6bbe4cad00e0b96f0b6ef568d9f08aafe1d4
jakkie <jakubkielbasiewicz@gmail.com>
2022-02-14 14:18:33 +0100

    Day 8 part 2 init

--- 8/solution.cpp
+++ 8/solution.cpp
@@ -2,6 +2,25 @@
 // 4 - bcdf
 // 7 - acf
 // 8 - abcdefg
+/*
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+*/
 
 #include <fstream>
 #include <string>
